0f5d47107b70958ab0835f6c5f06ea72201863b5
vjay7394 <zhl22pa@gmail.com>
2025-07-21 00:26:53 +0900

    Add devtools middleware to Zustand stores for enhanced debugging

--- src/main.jsx
+++ src/main.jsx
@@ -217,7 +217,7 @@ createRoot(document.getElementById('root')).render(
             {/* 개발 환경에서만 React Query Devtools 표시 */}
             {process.env.NODE_ENV === 'development' && (
               <ReactQueryDevtools 
-                initialIsOpen={false} 
+                initialIsOpen={true} 
                 position="bottom-right"
               />
             )}
